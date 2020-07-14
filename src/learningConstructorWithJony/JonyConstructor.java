00684c77b1855539632a792aa6da60e0018874d8
jonhossn <jonhossn@gmail.com>
2020-07-14 01:00:02 -0400

    final dreaft for construstor

--- src/learningConstructorWithJony/JonyConstructor.java
+++ src/learningConstructorWithJony/JonyConstructor.java
@@ -3,6 +3,7 @@ package learningConstructorWithJony;
 public class JonyConstructor {// class start
     String name;
     int age;
+    double rate;
 
    public JonyConstructor(){// this is default
        System.out.println("ny");
@@ -11,23 +12,25 @@ public class JonyConstructor {// class start
        this.name = name;//jony
        System.out.println(this.name);
    }
-    public JonyConstructor(String name, int age) {// rony----34
+    public JonyConstructor(String name, int age, double rate) {// rony----34
         this.name = name; //rony
-        this.age= age;// 34
-        System.out.println("name"+this.name+"age "+ this.age);
+        this.age= age;
+        this.rate=rate;//
+        System.out.println("name"+this.name+"age "+ this.age+" "+this.rate);
 
-    }
+    }//
+    // int a =;
     public JonyConstructor( int age, String name) {
         this.age = age;
         this.name= name;
-        System.out.println("name "+this.age+" age "+ this.name);
+        System.out.println(this.age+" age "+ this.name);
     }
 
 
     public static void main(String[] args) {
         JonyConstructor obj= new JonyConstructor();// this is to create object
         JonyConstructor obj2= new JonyConstructor(" jony");// passing argument
-        JonyConstructor obj3= new JonyConstructor("rony",34);
+        JonyConstructor obj3= new JonyConstructor("rony",34,3.4);
         JonyConstructor obj4= new JonyConstructor(22,"james bond");
 
         }
